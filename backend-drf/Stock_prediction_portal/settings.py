728a7d642be36215b37024c181863d889e7fcd65
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-08-17 12:00:10 +0530

    added allowed hosts and trusted origins to deploy

--- backend-drf/Stock_prediction_portal/settings.py
+++ backend-drf/Stock_prediction_portal/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = [
+    "https://stock-prediction-using-django.onrender.com",
+]
 
 
 # Application definition
